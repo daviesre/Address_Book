6fcdb44b5ba7109fb26fe6e5f862fd53946924a4
Russ Davies <russdavies392@gmail.com>
2016-07-15 12:17:44 -0700

    set up Contact class with private strings and getters and setters

--- Objects/address.cs
+++ Objects/address.cs
@@ -9,5 +9,43 @@ namespace AddressBook.Objects
     private string _name;
     private string _phoneNumber;
     private string _address;
+    private static List<Book> _instances = new List<Book> {};
+//Constructor
+    public Book (string name, string phone, string address)
+    {
+      _name = name;
+      _phoneNumber = phone;
+      _address = address;
+    }
+//Getters and Setters
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public string GetPhoneNumber()
+    {
+      return _phoneNumber;
+    }
+
+    public void SetPhoneNumber(string newPhoneNumber)
+    {
+      _phoneNumber = newPhoneNumber;
+    }
+
+    public string GetAddress()
+    {
+      return _address;
+    }
+
+    public void SetAddress(string address)
+    {
+      _address = newAddress;
+    }
   }
 }

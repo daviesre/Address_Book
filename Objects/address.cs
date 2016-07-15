cd2dcf3705598333e5096db76870411f78099b0e
Russ Davies <russdavies392@gmail.com>
2016-07-15 12:03:24 -0700

    create basic structure of address book, including set up of razor and nancy

---
+++ Objects/address.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace AddressBook.Objects
+{
+  public class Contact
+  {
+//Set strings
+    private string _name;
+    private string _phoneNumber;
+    private string _address;
+  }
+}
